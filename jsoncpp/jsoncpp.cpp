77b2acfabe7b390fc9dcb22f8e478bf8bc950596
NIL <moyi.tt@gmail.com>
2013-11-14 01:32:20 +0800

    add jsoncpp

---
+++ jsoncpp/jsoncpp.cpp
@@ -0,0 +1,42 @@
+/*
+* jsoncpp   http://jsoncpp.sourceforge.net/
+*
+* Makefile
+*   compile
+*       -I/path/to/json/include/path/
+*   link
+*       -L/path/to/json/lib/path/ -ljson
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <json/json.h>
+
+using namespace std;
+
+int main()
+{
+    Json::Reader reader;
+    Json::Value value;
+    string input;
+    value.clear();
+    if(reader.parse(input,value)){
+        string col1 = value["col1"].asString();
+        int col2 = default_col2;
+        try {
+            col2 = value["col2"].asInt();
+        }catch(std::exception &e){
+            catch_throw(e);
+        }
+
+        for(int i=0; i<value["outer"].size(); i++){
+            string col1 = value["outer"][i]["col1"].asString();
+            Value inner = value["outer"][i]["inner"];
+            for(int j=0; j<inner.size(); j++){
+                string col2 = inner[j]["col2"].asString();
+            }
+        }
+    } else
+        cout<<reader.getFormattedErrorMessages()<<endl;
+
+}
